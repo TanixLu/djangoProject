da679f45eea4fff9a783b1aa9eeb72f6a201d556
Tanix <mynameisltx@mail.ustc.edu.cn>
2021-07-25 22:10:33 +0800

    add views and urls and html

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-17pe%f*d-v#g#yn8$xve5mbj3=bl&gmkhnzzl5vv9a^&(0420_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
