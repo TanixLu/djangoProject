3875ba56a1f663757a76d8f1311c4ee6c081ba50
Tanix <mynameisltx@mail.ustc.edu.cn>
2021-07-25 20:53:48 +0800

    add 2 modules, Question and Choice

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -101,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
